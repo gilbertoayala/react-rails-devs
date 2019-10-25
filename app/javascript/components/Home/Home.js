c762301174f2d31a68d4771cea38e9e30c41b9fd
Gilberto Ayala <gayala@nearsoft.com>
2019-10-25 09:28:17 -0700

    finished jumbotron

---
+++ app/javascript/components/Home/Home.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import Jumbotron from './Jumbotron';
+class Home extends Component {
+  constructor() {
+    super();
+    //set our initial state here modules array that holds all the objects
+    this.state = {
+      course_modules: [
+        {
+          id: 1,
+          title: 'Setting up a new RoR with React',
+          description: 'lorem ipsum',
+          active: false
+        },
+        {
+          id: 2,
+          title: 'Adding React to an Existing Rails App',
+          description: 'lorem ipsum',
+          active: false
+        },
+        {
+          id: 3,
+          title: 'Building a Hello world app',
+          description: 'lorem ipsum',
+          active: false
+        },
+        {
+          id: 4,
+          title: 'Adding React Router DOM to your app',
+          description: 'lorem ipsum',
+          active: false
+        }
+      ]
+    };
+  }
+  render() {
+    return (
+      <div>
+        <Jumbotron></Jumbotron>
+      </div>
+    );
+  }
+}
+export default Home;
